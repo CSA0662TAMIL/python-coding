060f8eb7706f921fcc6b7770a9d7b1ff1999e713
CSA0662TAMIL <161743989+CSA0662TAMIL@users.noreply.github.com>
2024-03-03 19:40:20 +0530

    find greater number between three numbers

---
+++ greater.py
@@ -0,0 +1,9 @@
+i=eval(input("enter the first number :"))
+j=eval(input("enter the second number :"))
+k=eval(input("enter the third  number :"))
+if (i<k and j<k):
+       print(k,"is greater")
+elif(i<j and k<j):
+       print(j,"is greater")
+else:
+    print(i,"is greater")
